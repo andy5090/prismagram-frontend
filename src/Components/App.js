88ee95e437d5ca47f4fa0e731bc1d3bdb30538eb
Andy Lee <andy5090@naver.com>
2019-08-01 18:38:45 +0900

    chapter 5:Frontend Done

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { gql } from "apollo-boost";
+import styled, { ThemeProvider } from "styled-components";
+import { useQuery } from "react-apollo-hooks";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import GlobalStyles from "../Styles/GlobalStyles";
+import Theme from "../Styles/Theme";
+import AppRouter from "./Router";
+import Footer from "./Footer";
 
-function App() {
-  return <div className="App" />;
-}
+const QUERY = gql`
+  {
+    isLoggedIn @client
+  }
+`;
 
-export default App;
+const Wrapper = styled.div`
+  margin: 0 auto;
+  max-width: 935px;
+  width: 100%;
+`;
+
+export default () => {
+  const {
+    data: { isLoggedIn }
+  } = useQuery(QUERY);
+
+  return (
+    <ThemeProvider theme={Theme}>
+      <Wrapper>
+        <GlobalStyles />
+        <AppRouter isLoggedIn={isLoggedIn} />
+        <Footer />
+        <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
+      </Wrapper>
+    </ThemeProvider>
+  );
+};
